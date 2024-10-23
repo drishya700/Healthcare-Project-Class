92f5b2d53d842cd38f10f6d61b5f359ed3ca4581
drishya700 <110844521+drishya700@users.noreply.github.com>
2024-10-23 11:04:22 +0530

    Day 3 Commit

--- Server/Config/dbConnection.js
+++ Server/Config/dbConnection.js
@@ -1,9 +1,12 @@
 //mongoose module require maane package install karna padega
 
-const mongoose = require("mongoose";
-    const connectDb=async()=>{
-        try{
-            const connect = await mongoose.connect(process.env.CONNECTION_STRING);
-        }
+import mongoose from "mongoose";
+
+export const connectDb=async()=>{
+    try{
+        const connect = await mongoose.connect(process.env.CONNECTION_STRING)
+        console.log("Connected to mongo")
+    }catch(error){
+        console.log(error)
     }
-)
+}
